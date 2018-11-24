161f86e81815f312c7180e324721404cd93af935
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2018-11-23 18:07:09 -0600

    Spotify API Works

---
+++ liri.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+
+var request = process.argv[2];
+var parameter = process.argv[3];
+
+// Liri App Functions
+function userExp(){
+    switch (request){
+        case 'spotify-this-song':
+        findSong(parameter);
+        break;
+    }
+}
+
+// spotify-this-song API Call
+
+function findSong(parameter){
+    var searchSong;
+    if(parameter === undefined){
+        searchSong = "The Sign ace of base";
+    } else{
+        searchSong = parameter;
+    }
+spotify.search({
+    type: "track",
+    query: searchSong,
+}, function(err, data){
+    if(err){
+        console.log("ERROR!" + err);
+        return;
+    } else {
+        console.log("\n -------------------------------------\n");
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song: "+ data.tracks.items[0].name);
+        console.log("Preview: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("\n --------------------------------------\n");
+    }
+});
+};
+userExp();
