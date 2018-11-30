cb27120a74f65f29349246549148fc593263fef3
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2018-11-29 19:47:26 -0600

    Finished

--- liri.js
+++ liri.js
@@ -12,6 +12,7 @@ var moment = require ("moment");
 
 var command = process.argv[2];
 var parameter = process.argv[3];
+var searchMovie = "";
 
 // Liri App Functions
 function userExp() {
@@ -22,7 +23,22 @@ function userExp() {
         case 'concert-this':
             bandsInTown(parameter);
             break;
+        case 'movie-this':
+            OMDBCall(parameter);
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
     }
+}
+
+// Log everything that is input by the user
+
+function logIT(dataToLog){
+    console.log(dataToLog);
+    fs.appendFileSync("./log.txt", dataToLog, function(err){
+        if (err) console.log("An error occured!")
+    });
 }
 
 // bands in town API Call for concert-this
@@ -51,14 +67,41 @@ parameter +
     
     .then(function(response){
         for (var x = 0; x < response.data.length; x++){
-            console.log("=======================================");
-            console.log("Lineup: " + response.data[x].lineup);
-            console.log("Venue: " + response.data[x].venue.name);
-            console.log("Location: " + response.data[x].venue.city + " ," + response.data[x].venue.region + "," + response.data[x].venue.country);
-            console.log("Event Date: " + moment(response.data[x].datetime).format("MM/DD/YYYY"));
-            console.log("=======================================");
+            logIT("\n=======================================\n");
+            logIT("\nLineup: " + response.data[x].lineup + "\n");
+            logIT("\nVenue: " + response.data[x].venue.name + "\n");
+            logIT("\nLocation: " + response.data[x].venue.city + " ," + response.data[x].venue.region + "," + response.data[x].venue.country + "\n");
+            logIT("\nEvent Date: " + moment(response.data[x].datetime).format("MM/DD/YYYY") + "\n");
+            logIT("\n=======================================\n");
 
         }
+    })
+}
+
+// OMDB API Call
+
+function OMDBCall(parameter){
+    if(parameter === undefined){
+        parameter = "Mr. Nobody";
+        console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947");
+        console.log("\n It's on Netflix!\n")
+    } else{
+        searchMovie = parameter;
+    }
+
+    axios.get("http://www.omdbapi.com/?t=" +
+    parameter +
+    "&y=&plot=short&apikey=trilogy")
+    .then(function(response){
+        logIT("\n===================================\n");
+        logIT("\nTitle: " + response.data.Title + "\n");
+        logIT("\nYear: " + response.data.Year + "\n");
+        logIT("\nIMDB Rating: " + response.data.Ratings[0].Value + "\n");
+        logIT("\nRotten Tomatoes: " + response.data.Ratings[1].Value + "\n");
+        logIT("\nFilmed In: " + response.data.Country + "\n");
+        logIT("\nSummary: " + response.data.Plot + "\n");
+        logIT("\nActors: " + response.data.Actors + "\n");
+        logIT("\n===================================\n");
     })
 }
 
@@ -79,13 +122,36 @@ function findSong(parameter) {
             console.log("ERROR!" + err);
             return;
         } else {
-            console.log("\n========================================\n");
-            console.log("Artist: " + data.tracks.items[0].artists[0].name);
-            console.log("Song: " + data.tracks.items[0].name);
-            console.log("Preview: " + data.tracks.items[0].preview_url);
-            console.log("Album: " + data.tracks.items[0].album.name);
-            console.log("\n========================================\n");
+            logIT("\n========================================\n");
+            logIT("\nArtist: " + data.tracks.items[0].artists[0].name + "\n");
+            logIT("\nSong: " + data.tracks.items[0].name + "\n");
+            logIT("\nPreview: " + data.tracks.items[0].preview_url + "\n");
+            logIT("\nAlbum: " + data.tracks.items[0].album.name + "\n");
+            logIT("\n========================================\n");
         }
     });
 };
+
+// Run the do-what-it-says function
+function doWhatItSays(){
+    fs.readFile("random.txt", "utf-8", function(err, data){
+        if (err){
+            return console.log(err);
+        } else {
+            console.log(data);
+        }
+        var dataArr = data.split(",");
+        if (dataArr[0] === 'spotify-this-song'){
+            var songcheck = dataArr[1].trim().slice(1,-1);
+            findSong(songcheck);
+        }
+        else if (dataArr[0]=== 'concert-this'){
+            var bandName = dataArr[1]
+            bandsInTown(bandName);
+        } else if (dataArr[0] === "movie-this"){
+            var movie_name = dataArr[1].trim().slice(1,-1);
+            OMDBCall(movie_name);
+        }
+    })
+}
 userExp();
